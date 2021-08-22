1d42c73467a0bf08f5af260c3456b8f5320c599c
Erick Silva <erickbob.psp@hotmail.com>
2021-08-21 19:33:10 -0500

    Angular Components Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule} from '@angular/material/list';
+import { MatGridListModule} from '@angular/material/grid-list';
+import { MatCardModule} from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 
@@ -22,7 +25,10 @@ import { MenuComponent } from './menu/menu.component';
     FlexLayoutModule,
     MatToolbarModule, 
     AppRoutingModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
